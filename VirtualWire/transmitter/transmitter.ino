3752ca96240c764a2c474479b8cc5d0d2ac21859
Diego Ferreira da Silva <diego@MacBook-Pro-de-Diego.local>
2014-11-22 23:50:44 -0200

    alteracoes

--- VirtualWire/transmitter/transmitter.ino
+++ VirtualWire/transmitter/transmitter.ino
@@ -10,36 +10,32 @@
 
 #include <VirtualWire.h>
 
-const int led_pin = 11;
+const int led_trans_pin = 11;
 const int transmit_pin = 12;
-const int receive_pin = 2;
-const int transmit_en_pin = 3;
+const int receive_pin = 13;
 
 void setup()
 {
-    // Initialise the IO and ISR
     vw_set_tx_pin(transmit_pin);
     vw_set_rx_pin(receive_pin);
-    vw_set_ptt_pin(transmit_en_pin);
-    vw_set_ptt_inverted(true); // Required for DR3100
-    vw_setup(2000);       // Bits per sec
-    pinMode(led_pin, OUTPUT);
+    vw_setup(2000);
+    pinMode(led_trans_pin, OUTPUT);
 }
 
-byte count = 1;
+int count = 1;
 
 void loop()
 {
-  //char msg[7] = {'h','e','l','l','o',' ','#'};
+  
+  char msg[10];
+  
+  digitalWrite(led_trans_pin, HIGH);
 
-  //msg[6] = count;
+  sprintf(msg, "teste%d", count);
   
-  char *msg="teste";
-  
-  digitalWrite(led_pin, HIGH); // Flash a light to show transmitting
   vw_send((uint8_t *)msg, strlen(msg));
-  vw_wait_tx(); // Wait until the whole message is gone
-  digitalWrite(led_pin, LOW);
+  vw_wait_tx(); 
+  digitalWrite(led_trans_pin, LOW);
+  count++;
   delay(1000);
-  count = count + 1;
 }
