69cc0cadce77562109abfab57bf9a4ac499e0d8b
Diego <diegosiuniube@gmail.com>
2015-01-14 23:07:40 -0200

    alteracoes

--- rf_receiver_ino/rf_receiver_ino.ino
+++ rf_receiver_ino/rf_receiver_ino.ino
@@ -1,35 +1,31 @@
 #include <VirtualWire.h>
 
 const int led_rec_pin = 13;
-const int transmit_pin = 12;
+//const int transmit_pin = 12;
 const int receive_pin = 11;
-const int transmit_en_pin = 3;
+//const int transmit_en_pin = 3;
 const int relay_amp_pin = 10;
 
-char received[80];
 
+char received[20];
 void setup() {
     Serial.begin(9600);
-    vw_set_tx_pin(transmit_pin);
+    //vw_set_tx_pin(transmit_pin);
     vw_set_rx_pin(receive_pin);
     vw_setup(2000);
     vw_rx_start();
     pinMode(led_rec_pin, OUTPUT);
     pinMode(relay_amp_pin, OUTPUT);
-    
 }
 
 void loop() {
     
-    uint8_t buflen = 100;
+    uint8_t buflen = 20;
     uint8_t buf[buflen];
-    
-    
-    if (vw_get_message(buf, &buflen))
-    {
+ 
+    if (vw_get_message(buf, &buflen)){
         digitalWrite(led_rec_pin, HIGH);
-        for (int i = 0; i < buflen; i++)
-	{
+        for (int i = 0; i < buflen; i++){
 	    received[i]=buf[i];
 	}
         Serial.println(received);       
--- spot/spot.ino
+++ spot/spot.ino
@@ -1,4 +1,9 @@
 #include <VirtualWire.h>
+#include "DHT.h"
+
+#define DHTPIN A0
+#define DHTTYPE DHT11
+DHT dht(DHTPIN, DHTTYPE);
 
 const int transmit_pin = 12;
 const int receive_pin = 13;
@@ -7,15 +12,41 @@ void setup(){
     Serial.begin(9600);
     vw_set_tx_pin(transmit_pin);
     vw_set_rx_pin(receive_pin);
-    vw_setup(2000);
+    vw_setup(2000); 
+    dht.begin();
 }
 
 void loop(){
-    char msg2[80];
-    sprintf(msg2, "id:%s,nome:%s,valor:%s,teste:%s", "1","sala","40.32","51");
+    transmitTemperature();
+    delay(1000);
+    transmitHumidity();
+    delay(1000);
+}
+
+void transmitTemperature(){
+    float t = dht.readTemperature();
+    char temp[10];
+    dtostrf(t,6,2,temp);
+    char msg[40];
+    sprintf(msg, "%d,%s",1,temp);
+    //free(temp);
     
-    Serial.println(msg2); 
-    vw_send((uint8_t *)msg2, strlen(msg2));
+    Serial.println(msg); 
+    vw_send((uint8_t *)msg, strlen(msg));
     vw_wait_tx(); 
-    free(msg2);
+    //free(msg);
+}
+
+void transmitHumidity(){
+    float h = dht.readHumidity();
+    char temp[10];
+    dtostrf(h,6,2,temp);
+    char msg[40];
+    sprintf(msg, "%d,%s",2,temp);
+    //free(temp);
+    Serial.println(msg); 
+    vw_send((uint8_t *)msg, strlen(msg));
+    vw_wait_tx(); 
+   // free(msg);
+    
 }
