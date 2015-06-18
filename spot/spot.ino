9f28988009aa35353feae528de9adcc6a5c050a4
Diego Silva <diegosiuniube@gmail.com>
2015-06-17 23:39:25 -0300

    alteracoes

--- spot/spot.ino
+++ spot/spot.ino
@@ -1,18 +1,15 @@
-#include <VirtualWire.h>
+#include <SPI.h>
 #include "DHT.h"
+#include "nRF24L01.h"
+#include "RF24.h"
+#include "printf.h"
 
 #define DHTPIN A0
 #define DHTTYPE DHT11
 DHT dht(DHTPIN, DHTTYPE);
 
-const int transmit_pin = 12;
-const int receive_pin = 13;
-
 void setup(){
     Serial.begin(9600);
-    vw_set_tx_pin(transmit_pin);
-    vw_set_rx_pin(receive_pin);
-    vw_setup(2000); 
     dht.begin();
 }
 
@@ -32,8 +29,8 @@ void transmitTemperature(){
     //free(temp);
     
     Serial.println(msg); 
-    vw_send((uint8_t *)msg, strlen(msg));
-    vw_wait_tx(); 
+    //vw_send((uint8_t *)msg, strlen(msg));
+    //vw_wait_tx(); 
     //free(msg);
 }
 
@@ -45,8 +42,8 @@ void transmitHumidity(){
     sprintf(msg, "%d,%s",2,temp);
     //free(temp);
     Serial.println(msg); 
-    vw_send((uint8_t *)msg, strlen(msg));
-    vw_wait_tx(); 
+    //vw_send((uint8_t *)msg, strlen(msg));
+    //vw_wait_tx(); 
    // free(msg);
     
 }
