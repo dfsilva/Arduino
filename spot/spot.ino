b7a25af33ddb8b79c1788f23ccc45c5ed0ff6b2a
Diego Silva <diegosiuniube@gmail.com>
2015-06-22 22:51:24 -0300

    alteracoes

--- spot/spot.ino
+++ spot/spot.ino
@@ -8,9 +8,29 @@
 #define DHTTYPE DHT11
 DHT dht(DHTPIN, DHTTYPE);
 
+RF24 radio(9,10);
+
+//Topology
+const uint64_t pipes[2] = { 0xF0F0F0F0E1LL, 0xF0F0F0F0D2LL };
+
+
 void setup(){
     Serial.begin(9600);
     dht.begin();
+    radio.begin();
+
+    radio.setDataRate(RF24_250KBPS);
+    radio.setPALevel(RF24_PA_MAX);
+    radio.setChannel(70);
+
+    radio.enableDynamicPayloads();
+    radio.setRetries(15,15);
+    radio.setCRCLength(RF24_CRC_16);
+
+    radio.openWritingPipe(pipes[0]);
+    radio.openReadingPipe(1,pipes[1]);  
+
+    radio.startListening();
 }
 
 void loop(){
@@ -32,6 +52,14 @@ void transmitTemperature(){
     //vw_send((uint8_t *)msg, strlen(msg));
     //vw_wait_tx(); 
     //free(msg);
+
+    radio.openWritingPipe(pipes[1]);
+    radio.openReadingPipe(0,pipes[0]);
+    radio.stopListening();
+    bool ok = radio.write(&msg,strlen(msg));
+    radio.openWritingPipe(pipes[0]);
+    radio.openReadingPipe(1,pipes[1]); 
+    radio.startListening(); 
 }
 
 void transmitHumidity(){
