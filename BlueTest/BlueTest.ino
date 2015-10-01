c1bccb73b963588a2122beb05b6e8ee202c04935
Diego <diegosiuniube@gmail.com>
2015-09-30 23:47:18 -0300

    alteracoes

---
+++ BlueTest/BlueTest.ino
@@ -0,0 +1,22 @@
+#include <SoftwareSerial.h>
+#include "bluetooth.h"
+
+Bluetooth *blue = new Bluetooth("ExampleRobot");
+
+void setup() {
+  Serial.begin(9600);
+  blue->setupBluetooth();  
+}
+
+void loop() {
+  String msg = blue->Read();
+  if(msg.length() > 1){
+    Serial.println(msg);
+  }
+  
+  if(Serial.available()){
+    char reader = (char) Serial.read();
+    Serial.println(reader);
+    blue->Send(reader);
+  }
+}
