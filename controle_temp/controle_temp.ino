1eb71799ee0161e4ff4487081692e2b700f905b6
Diego Ferreira da Silva <diego@diegosilva.com.br>
2018-10-02 13:51:58 -0300

    fixes

--- controle_temp/controle_temp.ino
+++ controle_temp/controle_temp.ino
@@ -1,25 +1,26 @@
-#include <dht.h>
 #include <IRremote.h>
 #include <MideaIR.h>
+#include "Si7021.h"
 
-dht DHT;
-#define DHT11_PIN 7
+Si7021 si7021;
+
 #define IR_EMITER 3
 
 #define SIXT_SECONDS 60000
 #define TEN_SECONDS  10000
 #define FIVE_SECONDS 5000
 
-#define IDEAL_TEMP 24
+#define IDEAL_TEMP 24.00
 
-int lastTemp = 18;
+int lastTemp = IDEAL_TEMP;
 
 IRsend irsend;
 MideaIR remote_control(&irsend);
 
 void setup(){
   Serial.begin(9600);
-  // Define IR PIN as Output
+  si7021.begin();
+
   pinMode(IR_EMITER, OUTPUT);
   
   remote_control.turnON();
@@ -36,23 +37,23 @@ void loop()
 }
 
 void checkTemperature(){
-   delay(FIVE_SECONDS);
-   int chk = DHT.read11(DHT11_PIN);
-   double temperature = DHT.temperature;
-   Serial.print("Temperature = ");
-   Serial.println(temperature);
-   double humidity = DHT.humidity;
-   Serial.print("Humidity = ");
+   delay(SIXT_SECONDS);
+   double humidity = si7021.measureHumidity();
+   Serial.print("Humidity DH11= ");
    Serial.println(humidity);
+   
+   double temperature = si7021.getTemperatureFromPreviousHumidityMeasurement();
+   Serial.print("Temperature DH11 = ");
+   Serial.println(temperature);
 
-  if((int)temperature > IDEAL_TEMP){
-    if((int)lastTemp > 17){
+  if(temperature > IDEAL_TEMP){
+    if(lastTemp > 17){
        lastTemp = lastTemp - 1;
     }
     Serial.print("Setando a temperatura reduzida = ");
     Serial.println(lastTemp);
   }else{
-    if((int)lastTemp < 30){
+    if(lastTemp < 30){
       lastTemp = lastTemp + 1;
     }
     Serial.print("Setando a temperatura aumentada = ");
